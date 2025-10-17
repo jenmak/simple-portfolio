beb7d07718b79eaace7f8549f2d176bea45cbdd4
Jen Mak <jennifer.michelle.mak@gmail.com>
2025-10-17 10:09:50 -0400

    Preliminary version for publish

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,10 +1,13 @@
 export default function Portfolio() {
 
   return (
-    <div className="h-screen w-full h-screen bg-background">
-      <div className="md:w-1/2 md:max-w-2xl mx-auto p-10 md:py-30">
-        <h1 className="text-left font-serif text-foreground text-6xl relative name mb-2">
-          <span className="text-foreground jen">Jen</span>
+    <div className="h-screen w-full h-screen bg-background relative">
+      <div className="layer layer-window"></div>
+      <div className="layer layer-swatch-1"></div>
+      <div className="layer layer-swatch-2"></div>
+      <div className="layer layer-name">
+        <h1 className="text-left font-serif text-6xl relative name mb-8">
+          <span className="jen">Jen</span>
           <span className="nifer">
             <span className="letter-animate letter-1">n</span>
             <span className="letter-animate letter-2">i</span>
@@ -12,25 +15,35 @@ export default function Portfolio() {
             <span className="letter-animate letter-4">e</span>
             <span className="letter-animate letter-5">r</span>
           </span>
-          <span className="text-foreground mak">Mak</span>
+          <span className="mak">Mak</span>
         </h1>
       </div>
-      {/* <h3 className="text-foreground text-2xl font-serif mb-2">
-          Senior <span className="text-accent-2">UX</span> Engineer.
-        </h3> */}
-      {/* <span className="text-accent-4">Currently</span> Freelance.</h3>
-      <h3 className="text-foreground text-2xl font-serif mb-2">
-        <span className="text-accent-1">Formerly</span> Grubhub & Unqork.
-      </h3>
-      <h3 className="text-foreground text-2xl font-serif mb-2">
-        <h3 className="text-foreground text-2xl font-serif mb-2">Articles</h3>
-        <a href="https://www.builtinnyc.com/articles/software-engineers-solved-technical-challenges">
-          <h3 className="text-foreground text-2xl font-serif mb-2">Built in NYC</h3>
+      <div className="layer layer-socials">
+        <a href="https://github.com/jenmak">
+          <img className="social-icon" src="/github.svg" alt="Github" />
         </a>
-        <h3>Projects</h3>
-        <a href="https://dripdropcityfrontend-production.up.railway.app/">
-          <h3 className="text-foreground text-2xl font-serif mb-2">Drip Drop City</h3>
-        </a> */}
+        <a href="https://www.linkedin.com/in/jennifermichellemak/">
+          <img className="social-icon" src="/linkedin.svg" alt="LinkedIn" />
+        </a>
+        <a href="/JenMak_Resume_2025.pdf">
+          <img className="social-icon" src="/resume.svg" alt="Resume" />
+        </a>
+      </div>
+      <div className="layer layer-content font-serif">
+        <h2 className="text-foreground text-xl mb-2">Senior UX Engineer</h2>
+        <h2 className="text-md mb-6"><span className="text-accent-2">Formerly</span> Grubhub & Unqork.</h2>
+        <h4 className="text-accent-3 text-sm mb-3 font-sans">Apps</h4>
+        <a target="_blank" href="https://dripdropcityfrontend-production.up.railway.app/">
+          <h3 className="text-md mb-3 hover:text-white">Drip Drop City</h3>
+        </a>
+        <a target="_blank" href="">
+          <h3 className="text-md mb-3 hover:text-white">Permit Flow</h3>
+        </a>
+        <h4 className="text-accent-3 text-sm mb-3 font-sans">Articles</h4>
+        <a target="_blank" href="https://www.builtinnyc.com/articles/software-engineers-solved-technical-challenges">
+          <h3 className="text-md mb-3 hover:text-white">Built in NYC: Cracking the Case; How 3 Software Engineers Solved Their Biggest Technical Challenges</h3>
+        </a>
+      </div>
     </div>
   )
 }
